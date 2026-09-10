8d2c37409f405d5ab6140b183cfe930a5b062764
enesuzun2002 <emurat.uzun@gmail.com>
2026-09-10 19:13:10 +0300

    Initial Commit

---
+++ CoinFlow/CoinFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFlowApp.swift
+//  CoinFlow
+//
+//  Created by Enes Murat Uzun on 10.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct CoinFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinFlowTests/CoinFlowTests.swift
@@ -0,0 +1,19 @@
+//
+//  CoinFlowTests.swift
+//  CoinFlowTests
+//
+//  Created by Enes Murat Uzun on 10.09.2026.
+//
+
+import Testing
+@testable import CoinFlow
+
+struct CoinFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
